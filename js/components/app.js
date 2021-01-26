a45f8456ce175dded4ca4e3b91ce56fa6bfd7c0f
Reece Devine <reece.devine97@gmail.com>
2021-01-26 10:54:17 -0330

    Adding content

--- js/components/app.js
+++ js/components/app.js
@@ -2,7 +2,7 @@ const http = require('http'); //just like a php require
 
 //require is more or less the same as a JS import
 
-const hostname = '127.0.0.1'; //hard coded localhost 
+//const hostname = '127.0.0.1'; //hard coded localhost 
 const port = process.env.PORT || 3000; //to view this app in browser type Localhost: 3000
 
 const server = http.createServer((req, res) => {
@@ -11,6 +11,6 @@ const server = http.createServer((req, res) => {
   res.end('Im sorry too');
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(port, () => {
+  console.log(`Server running at ${port}/`);
 });
