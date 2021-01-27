c590c9c9262032d80c47bc2cc29ac3103e1b0920
Reece Devine <reece.devine97@gmail.com>
2021-01-27 12:22:27 -0330

    updating

--- js/components/app.js
+++ js/components/app.js
@@ -1,16 +1,17 @@
-const http = require('http'); //just like a php require
+const http = require('http'); /// just like a php require
 
-//require is more or less the same as a JS import
+// require is more or less like a js import
 
-//const hostname = '127.0.0.1'; //hard coded localhost 
-const port = process.env.PORT || 3000; //to view this app in browser type Localhost: 3000
+//const hostname = '127.0.0.1'; //this is localhost
+const port = process.env.PORT || 3000; //the port number
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Im sorry too');
+  res.end('Hello WOOOOOOOOOORRRLLLLDD!');
 });
 
 server.listen(port, () => {
-  console.log(`Server running at ${port}/`);
+  console.log(`Server is now running at ${port}/`);
 });
+
