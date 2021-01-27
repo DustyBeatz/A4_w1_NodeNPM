cabf28121486c620bfdd8a93e6c4a68efeda1c7c
Reece Devine <reece.devine97@gmail.com>
2021-01-27 16:38:35 -0330

    adding express generator

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 const express = require('express'); /// just like a php require
-
+const path = require('path');
 const server = express();
 
+//set our view directory
+server.set('views', path.join(__dirname, 'views'));
+// set the static assets director so express knows where to look
+//for css files, JSfiles, images etc.. -anything static
+server.use(express.static(path.join(__dirname, 'public')));
 
-server.get("/", (req, res) => {
+server.get('/', (req, res) => {
   console.log('git the home route');
-    res.send('your first express ass');
-})
-
-server.get("/contact", (req, res) => {
-  console.log('hit the contact');
-    res.send('hit that contact page son');
+  res.sendFile('index.html');
 });
+
+server.get('/contact', (req, res) => {
+  console.log('hit the contact');
+    res.render('contact');
+});
+
 
 const port = process.env.PORT || 3000; //the port number
 
